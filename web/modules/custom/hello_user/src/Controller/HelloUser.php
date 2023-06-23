fc238d68739554c87b1395cf7d113b50ad71f930
souvik <souvik.banerjee@innoraft.com>
2023-06-23 16:41:39 +0530

    FT2023-339: Adding comments and coding standards fixing.

--- web/modules/custom/hello_user/src/Controller/HelloUser.php
+++ web/modules/custom/hello_user/src/Controller/HelloUser.php
@@ -3,8 +3,17 @@
 namespace Drupal\hello_user\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Class HelloUser
+ * extends ControllerBase and contains functionality for module.
+ */
 class HelloUser extends ControllerBase {
   
+  /**
+   *  Function view to render content with account name with hello-user theme.
+   * 
+   *  @return array with $content.
+   */
   public function view() {
     $content = [];
     $content['name'] = \Drupal::currentUser()->getAccountName();
