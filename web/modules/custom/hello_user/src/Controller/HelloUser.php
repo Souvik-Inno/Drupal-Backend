6018672b0c1af578594607abe1503165044a8aa5
souvik <souvik.banerjee@innoraft.com>
2023-06-29 23:52:21 +0530

    FT2023-340: Adding cache  configuration for site name changes.

--- web/modules/custom/hello_user/src/Controller/HelloUser.php
+++ web/modules/custom/hello_user/src/Controller/HelloUser.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\hello_user\Controller;
 
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\hello_user\CurrentUser;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -50,10 +51,14 @@ class HelloUser extends ControllerBase {
    */
   public function view() {
     $this->content['name'] = $this->currentUser->getUserDisplayName();
-
+    $var = $this->currentUser->getUserCacheTags();
+    
     return [
       '#theme' => 'hello_user',
       '#content' => $this->content,
+      '#cache' => [
+        'tags' => $this->currentUser->getUserCacheTags(),
+      ],
     ];
   }
 
