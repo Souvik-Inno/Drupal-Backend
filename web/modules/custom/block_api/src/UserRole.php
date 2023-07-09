e2c2ef81297e413cb4f61cd322924ece04f6f6cc
souvik <souvik.banerjee@innoraft.com>
2023-07-09 16:13:22 +0530

    FT2023-343: Build Custom Block to Welcome User.

---
+++ web/modules/custom/block_api/src/UserRole.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\block_api;
+
+use Drupal\Core\Session\AccountProxyInterface;
+
+/**
+ * Gets User's Role.
+ */
+class UserRole {
+
+  /**
+   * The current user.
+   * 
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new CurrentUser object.
+   * 
+   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
+   *   The current user service.
+   */
+  public function __construct(AccountProxyInterface $currentUser) {
+    $this->currentUser = $currentUser;
+  }
+
+  /**
+   * Gets the current user's display name.
+   * 
+   * @return \Drupal\user\Entity\Role
+   *   The user's role.
+   */
+  public function getUserRole() {
+    $roles = $this->currentUser->getRoles();
+    return $roles[array_key_last($roles)];
+  }
+
+  /**
+   * Gets the cache tag for user.
+   * 
+   * @return string[]
+   *   The cache tag.
+   */
+  public function getUserCacheTags() {
+    $cacheTags = ['user:' . $this->currentUser->id()];
+    return $cacheTags;
+  }
+
+}
