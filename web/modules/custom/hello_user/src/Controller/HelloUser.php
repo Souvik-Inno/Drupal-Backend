e2d975d0d418307a3be664a762b1f91fcd885285
souvik <souvik.banerjee@innoraft.com>
2023-07-10 18:52:33 +0530

    FT2023-340: Removing services and config set.

--- web/modules/custom/hello_user/src/Controller/HelloUser.php
+++ web/modules/custom/hello_user/src/Controller/HelloUser.php
@@ -3,38 +3,11 @@
 namespace Drupal\hello_user\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\hello_user\CurrentUser;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Returns responses for hello user content routes.
  */
 class HelloUser extends ControllerBase {
-  /**
-   * The current user.
-   *
-   * @var \Drupal\hello_user\CurrentUser
-   */
-  protected $currentUser;
-
-  /**
-   * Constructs new HelloUser controller object.
-   *
-   * @param \Drupal\hello_user\CurrentUser $currentUser
-   *   The current user service.
-   */
-  public function __construct(CurrentUser $currentUser) {
-    $this->currentUser = $currentUser;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('hello_user.current_user'),
-    );
-  }
 
   /**
    * Renders a page to view user name.
@@ -45,9 +18,9 @@ class HelloUser extends ControllerBase {
   public function view() {
     return [
       '#theme' => 'hello_user',
-      '#content' => $this->currentUser->getUserDisplayName(),
+      '#content' => \Drupal::currentUser()->getDisplayName(),
       '#cache' => [
-        'tags' => $this->currentUser->getUserCacheTags(),
+        'tags' => ["user:" . \Drupal::currentUser()->id()],
       ],
     ];
   }
