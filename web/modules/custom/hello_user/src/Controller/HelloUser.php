3099f4f54c73821d076982f6e5c030a7f6d37e68
souvik <souvik.banerjee@innoraft.com>
2023-07-11 10:10:24 +0530

    FT2023-340: Fixing code to use core services in dependency injection.

--- web/modules/custom/hello_user/src/Controller/HelloUser.php
+++ web/modules/custom/hello_user/src/Controller/HelloUser.php
@@ -3,11 +3,38 @@
 namespace Drupal\hello_user\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Returns responses for hello user content routes.
  */
 class HelloUser extends ControllerBase {
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs new HelloUser controller object.
+   *
+   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
+   *   The current user service.
+   */
+  public function __construct(AccountProxyInterface $currentUser) {
+    $this->currentUser = $currentUser;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user'),
+    );
+  }
 
   /**
    * Renders a page to view user name.
@@ -18,9 +45,9 @@ class HelloUser extends ControllerBase {
   public function view() {
     return [
       '#theme' => 'hello_user',
-      '#content' => \Drupal::currentUser()->getDisplayName(),
+      '#content' => $this->currentUser->getDisplayName(),
       '#cache' => [
-        'tags' => ["user:" . \Drupal::currentUser()->id()],
+        'tags' => ['user:' . $this->currentUser->id()],
       ],
     ];
   }
