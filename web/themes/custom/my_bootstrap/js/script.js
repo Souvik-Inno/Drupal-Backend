05f34aa966c8d47cc59d9e929218a87b92b62a6f
souvik <souvik.banerjee@innoraft.com>
2023-08-15 12:42:42 +0530

    FT2023-353: Exploring Views API.

--- web/themes/custom/my_bootstrap/js/script.js
+++ web/themes/custom/my_bootstrap/js/script.js
@@ -1,9 +1,6 @@
 (function ($, Drupal) {
   Drupal.behaviors.myBootstrapBehavior = {
     attach: function(context, settings) {
-      $('#page-text-change', context).click(function() {
-        $('#page-text', context).val(Math.random());
-      });
       $('#phone', context).on('input', function() {
         var input = $(this).val();
         if (input.length === 10) {
